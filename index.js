91061ec1342b0313095b4e96029c678b3d321b99
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-07-10 00:02:42 +0530

    add new files

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require ('express');
+const JWT_SECRET = "khushiisalwayshappy"
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-//should return a random long string
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// -------------------------------------------------------------------------------
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//should return a random long string
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
+
+// ---------------------------------------------------------------------------------
 
 app.post ("/signup" , function (req, res) {
     // res.json ({
@@ -46,9 +52,14 @@ app.post ("/signin" , function (req, res) {
         return u.username === username && u.password === password;
     });
 
+
+    //  Function signature of jwt.sign , it's takes two arguments 1. what you want encode or encrypt (username) , 2.what is your secret that u r using to sign the specific token or create specific tokens  
     if (foundUser) {
-        const token = generateToken();
-        foundUser.token = token;
+        const token = jwt.sign({
+            username : username
+        }, JWT_SECRET );
+
+        // foundUser.token = token; // no need to store it in the memory anymore 
 
         res.json({
             message: "Sign in successful",
